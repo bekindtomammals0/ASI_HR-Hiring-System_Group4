65f017b43598cf6f6dee3769eb0c01832bb3069a
Adrian Jay Barcenilla <111829440+adrianjaybarcenilla@users.noreply.github.com>
2023-07-18 13:18:09 +0800

    Add scripts for creating database tables

---
+++ Basecode.Data/SQL/Scripts/Create dbo.CharacterReference.sql
@@ -0,0 +1,22 @@
+﻿USE [BaseCodeDB]
+GO
+
+/* Avengers! Please take note that 
+the name of the DB being used in line 1 is correct.
+Thank you! 
+-Barcenilla, AJ*/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[CharacterReference] (
+    [ID]        INT           NOT NULL,
+    [email]     VARCHAR (255) NOT NULL,
+    [firstName] VARCHAR (255) NOT NULL,
+    [lastName]  VARCHAR (255) NOT NULL,
+    [responded] BIT           NOT NULL
+);
+
+
