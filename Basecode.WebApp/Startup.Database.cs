9f5dc39b5d5e385710275e22821dbb4a4c815b93
Bµlawan <brianbulawan5@gmail.com>
2023-07-18 09:10:43 +0800

    Improved unified connection string and added it to Startup1.Database.cs and Startup.Database.cs

--- Basecode.WebApp/Startup.Database.cs
+++ Basecode.WebApp/Startup.Database.cs
@@ -8,24 +8,29 @@ namespace Basecode.WebApp
     {
         private void ConfigureDatabase(IServiceCollection services)
         {
-            string server = Environment.GetEnvironmentVariable("DB_SERVER");
+            string server = Environment.GetEnvironmentVariable("DB_NAME");
             string database = Environment.GetEnvironmentVariable("DB_NAME");
             string username = Environment.GetEnvironmentVariable("DB_USER");
             string password = Environment.GetEnvironmentVariable("DB_PASSWORD");
-            string connectionString = $"Server={server};Database={database};User Id={username};Password={password};Trusted_Connection=True;MultipleActiveResultSets=true";
 
-
-            services.AddDbContext<BasecodeContext>(
-            options =>
-                options.UseSqlServer(
-                    connectionString, 
-                    optionsAction => { }
-                )
-                // options.UseSqlServer(
-                //     Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found."),
-                //     optionsAction => { }
-                // )
-            );
+            if (!string.IsNullOrEmpty(server) && !string.IsNullOrEmpty(database) && !string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                // All environment variables are set, use them to construct the connection string
+                string connectionString = $"Server={server};Database={database};User Id={username};Password={password};Trusted_Connection=True;MultipleActiveResultSets=true";
+                services.AddDbContext<BasecodeContext>(options =>
+                    options.UseSqlServer(connectionString, optionsAction => { })
+                );
+            }
+            else
+            {
+                // Use the default connection string configuration from appsettings.json or throw an exception if not found
+                services.AddDbContext<BasecodeContext>(options =>
+                    options.UseSqlServer(
+                        Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found."),
+                        optionsAction => { }
+                    )
+                );
+            }
 
             services.AddIdentity<IdentityUser, IdentityRole>()
                     .AddRoles<IdentityRole>()
