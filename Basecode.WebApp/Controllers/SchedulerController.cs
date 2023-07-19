5c146742101685796fc48e84e16da48ce1cefa0b
Queen Riza Montayre <111752643+montayreqr@users.noreply.github.com>
2023-07-19 12:32:40 +0800

    Create HR appointment scheduler page (#15)
    * Create controller for HR appointment scheduler page
    
    * Create javascript for HR appointment scheduler page
    
    * Add styles for HR appointment scheduler page
    
    * Modify margin for table
    
    * Create javascript for pagination of table
    
    * Add styles for pagination of table
    
    * Create view for HR appointment scheduler
    
    * Modify some margin in Hr appointment scheduler page
    
    * Remove unnecessary comments

---
+++ Basecode.WebApp/Controllers/SchedulerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class SchedulerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
