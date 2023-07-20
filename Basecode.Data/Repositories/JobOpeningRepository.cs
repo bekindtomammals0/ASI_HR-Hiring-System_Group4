6f6f82fb7165982d9741be0461034c91b58f5b31
adrianjaybarcenilla <adrianjay.barcenilla@cit.edu>
2023-07-20 19:41:29 +0800

    Add documentation to JobOpeningRepository

--- Basecode.Data/Repositories/JobOpeningRepository.cs
+++ Basecode.Data/Repositories/JobOpeningRepository.cs
@@ -8,36 +8,66 @@ using System.Threading.Tasks;
 
 namespace Basecode.Data.Repositories
 {
+    /// <summary>
+    /// Repository class to interact with the 'JobOpening' entity in the database.
+    /// </summary>
     public class JobOpeningRepository : BaseRepository, IJobOpeningRepository
     {
         private readonly BasecodeContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JobOpeningRepository"/> class.
+        /// </summary>
+        /// <param name="unitOfWork">The unit of work instance used for transaction management.</param>
+        /// <param name="context">The database context instance.</param>
         public JobOpeningRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Retrieves all Job Openings from the database.
+        /// </summary>
+        /// <returns>An <see cref="IQueryable{JobOpening}"/> containing all Job Openings.</returns>
         public IQueryable<JobOpening> RetrieveAll()
-        { 
+        {
             return this.GetDbSet<JobOpening>();
         }
 
+        /// <summary>
+        /// Adds a new Job Opening to the database.
+        /// </summary>
+        /// <param name="jobOpening">The Job Opening entity to be added.</param>
         public void Add(JobOpening jobOpening)
-        { 
+        {
             _context.JobOpening.Add(jobOpening);
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Retrieves a Job Opening from the database based on its ID.
+        /// </summary>
+        /// <param name="id">The ID of the Job Opening to retrieve.</param>
+        /// <returns>The Job Opening entity if found; otherwise, null.</returns>
         public JobOpening GetById(int id)
         {
             return _context.JobOpening.Find(id);
         }
 
-        public void Update(JobOpening jobOpening) 
+        /// <summary>
+        /// Updates an existing Job Opening in the database.
+        /// </summary>
+        /// <param name="jobOpening">The updated Job Opening entity.</param>
+        public void Update(JobOpening jobOpening)
         {
             _context.JobOpening.Update(jobOpening);
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Deletes a Job Opening from the database based on its ID.
+        /// </summary>
+        /// <param name="id">The ID of the Job Opening to delete.</param>
         public void Delete(int id)
         {
             var data = _context.JobOpening.Find(id);
