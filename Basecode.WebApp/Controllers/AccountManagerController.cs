d387c4463c18cf56ee0ffcff5dead3ab0cdfc1c5
HonjoCisco4 <124168603+HonjoCisco4@users.noreply.github.com>
2023-07-21 09:35:05 +0800

    Added Basic Authentication, Authorization, Registration, Login
    Added Basic Authentication, Authorization, Registration, Login

---
+++ Basecode.WebApp/Controllers/AccountManagerController.cs
@@ -0,0 +1,73 @@
+﻿using Basecode.Data.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class AccountManagerController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        //constructor
+        public AccountManagerController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationViewModel registrationViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var userToRegister = new IdentityUser { UserName = registrationViewModel.UserName, Email = registrationViewModel.Email };
+                var result = await userManager.CreateAsync(userToRegister, registrationViewModel.Password);
+
+                if(result.Succeeded)
+                {
+                    await signInManager.SignInAsync(userToRegister, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return RedirectToAction("RegisterView");
+        }
+        [HttpPost]
+        public async Task<IActionResult> logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password, loginViewModel.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+            return RedirectToAction("LoginView");
+        }
+        [HttpGet]
+        public IActionResult RegisterView()
+        {
+            return View("~/Areas/Identity/Pages/Account/Register.cshtml");
+        }
+        [HttpGet]
+        public IActionResult LoginView()
+        {
+            return View("~/Areas/Identity/Pages/Account/Login.cshtml");
+        }
+    }
+}
--- Basecode.WebApp/Controllers/JobOpeningController.cs
+++ Basecode.WebApp/Controllers/JobOpeningController.cs
@@ -2,58 +2,57 @@
 using Basecode.Data.ViewModels;
 using Basecode.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basecode.WebApp.Controllers
 {
-    [Authorize(AuthenticationSchemes = "Bearer", Roles = "User")]
     [AllowAnonymous]
     public class JobOpeningController : Controller
     {
         private readonly IJobOpeningService _service;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
         public JobOpeningController(IJobOpeningService service) 
         { 
             _service = service;
         }
+        [AllowAnonymous]
         public IActionResult Index()
         {
             var data = _service.RetrieveAll();
             return View(data);
         }
-
         public IActionResult AddView() 
         {
             return View();
         }
-
         [HttpPost]
         public IActionResult Add(JobOpening model)
         {
+            model.CreatedBy = User.Identity.Name;
+            model.UpdatedBy = User.Identity.Name;
             _service.Add(model);
             return RedirectToAction("Index");
         }
-
         [HttpGet]
         public IActionResult UpdateView(int id)
         {
             var data = _service.GetById(id);
             return View(data);
         }
-
         [HttpPost]
         public IActionResult Update(JobOpening model)
         {
             _service.Update(model);
             return RedirectToAction("Index");
         }
-
         [HttpGet]
         public IActionResult DeleteView(int id)
         {
             var data = _service.GetById(id);
             return View(data);
         }
-
         [HttpPost]
         public IActionResult Delete(int id)
         {
