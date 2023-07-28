5d5ca49384f0cbefefa921ff042c8c7ce8547379
adrianjaybarcenilla <adrianjay.barcenilla@cit.edu>
2023-07-28 13:46:17 +0800

    Create ICharacterReference repository

---
+++ Basecode.Data/Interfaces/ICharacterReferenceRepository.cs
@@ -0,0 +1,18 @@
+﻿using Basecode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Interfaces
+{
+    public interface ICharacterReferenceRepository
+    {
+        IQueryable<CharacterReference> RetrieveAll();
+        void Add(CharacterReference characterReference);
+        CharacterReference GetById(int id);
+        void Update(CharacterReference characterReference);
+        void Delete(int id);
+    }
+}
