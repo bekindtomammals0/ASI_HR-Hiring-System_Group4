6837e6e62773f12e3c220a7906268c40690fb4a4
adrianjaybarcenilla <adrianjay.barcenilla@cit.edu>
2023-07-31 15:47:57 +0800

    Create ICVService

---
+++ Basecode.Services/Interfaces/ICVService.cs
@@ -0,0 +1,19 @@
+﻿using Basecode.Data.Models;
+using Basecode.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface ICVService
+    {
+        List<CV> RetrieveAll();
+        void Add(CV cv);
+        CV GetById(int id);
+        void Update(CV cv);
+        void Delete(int id);
+    }
+}
