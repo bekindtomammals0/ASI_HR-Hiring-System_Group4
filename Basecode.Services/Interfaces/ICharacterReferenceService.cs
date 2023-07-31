9f78bda4d3a03bca0f7fc6ba9ffc1b568331c88f
adrianjaybarcenilla <adrianjay.barcenilla@cit.edu>
2023-07-31 15:47:45 +0800

    Create ICharacterReferenceService

---
+++ Basecode.Services/Interfaces/ICharacterReferenceService.cs
@@ -0,0 +1,19 @@
+﻿using Basecode.Data.Models;
+using Basecode.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface ICharacterReferenceService
+    {
+        List<CharacterReference> RetrieveAll();
+        void Add(CharacterReference characterReference);
+        CharacterReference GetById(int id);
+        void Update(CharacterReference characterReference);
+        void Delete(int id);
+    }
+}
