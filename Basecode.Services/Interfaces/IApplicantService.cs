31b5d142a9bd771a4da703d5c8c89beb90e5e797
adrianjaybarcenilla <adrianjay.barcenilla@cit.edu>
2023-07-31 15:47:34 +0800

    Create IApplicantService

---
+++ Basecode.Services/Interfaces/IApplicantService.cs
@@ -0,0 +1,19 @@
+﻿using Basecode.Data.Models;
+using Basecode.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface IApplicantService
+    {
+        List<Applicant> RetrieveAll();
+        void Add(Applicant applicant);
+        Applicant GetById(int id);
+        void Update(Applicant applicant);
+        void Delete(int id);
+    }
+}
