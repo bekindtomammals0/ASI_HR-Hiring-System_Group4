27685678221380aaa7b83f7591056a423c9e0cf1
adrianjaybarcenilla <adrianjay.barcenilla@cit.edu>
2023-07-31 11:16:40 +0800

    Update BasecodeContext

--- Basecode.Data/BasecodeContext.cs
+++ Basecode.Data/BasecodeContext.cs
@@ -22,12 +22,15 @@ namespace Basecode.Data
             RefreshToken.Add(token);
             SaveChanges();
         }
+        public virtual DbSet<Applicant> Applicant { get; set; }
         public virtual DbSet<CharacterReference> CharacterReference { get; set; }
         public virtual DbSet<CivilStatus> CivilStatus { get; set; }
-        public virtual DbSet<JobOpening> JobOpening { get; set; }
-        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<CV> CV { get; set; }
         public virtual DbSet<ExperienceLevel> ExperienceLevel {get; set; }
-        public virtual DbSet<EmploymentType> EmploymentType { get; set; }   
+        public virtual DbSet<EmploymentType> EmploymentType { get; set; }
+        public virtual DbSet<JobOpening> JobOpening { get; set; }
+        public virtual DbSet<ProcessStatus> ProcessStatus { get; set; }
         public virtual DbSet<RefreshToken> RefreshToken { get; set; }
+        public virtual DbSet<User> User { get; set; }
     }
 }
