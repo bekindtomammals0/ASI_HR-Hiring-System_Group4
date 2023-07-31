16bb1b9c6771ede512633943db72f7b918dcfa9f
adrianjaybarcenilla <adrianjay.barcenilla@cit.edu>
2023-07-31 11:14:53 +0800

    Create IApplicantRepository

---
+++ Basecode.Data/Interfaces/IApplicantRepository.cs
@@ -0,0 +1,18 @@
+﻿using Basecode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Interfaces
+{
+    public interface IApplicantRepository
+    {
+        IQueryable<Applicant> RetrieveAll();
+        void Add(Applicant applicant);
+        Applicant GetById(int id);
+        void Update(Applicant applicant);
+        void Delete(int id);
+    }
+}
