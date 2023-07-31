bcecaa21e7328df05dd6b42389c46d97c1a1cb41
adrianjaybarcenilla <adrianjay.barcenilla@cit.edu>
2023-07-31 11:15:25 +0800

    Create Applicant model

---
+++ Basecode.Data/Models/Applicant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Basecode.Data.Models
+{
+    public class Applicant
+    {
+        public int Id { get; set; }
+        public int UserID { get; set; }
+        public int CivilStatusID { get; set; }
+        public int? CVId { get; set; }
+        public int? JobOpeningID { get; set; }
+        public int? CharacterReferenceID { get; set; }
+        public int? ProcessStatusID { get; set; }
+        public bool CharacterReferenceResponded { get; set; }
+        public string ContactNo { get; set; }
+
+        public User User { get; set; }
+        public CivilStatus CivilStatus { get; set; }
+        public CV CV { get; set; }
+        public JobOpening JobOpening { get; set; }
+        public CharacterReference CharacterReference { get; set; }
+        public ProcessStatus ProcessStatus { get; set; }
+    }
+}
